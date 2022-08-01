7283761f2d447df7079cc95e1ad827e66bb1efc7
irakli-dadu <lipsowow@gmail.com>
2022-08-01 18:37:18 +0400

    fastfast
    ...

---
+++ script.js
@@ -0,0 +1,70 @@
+//------------------------------1---------------------------------
+
+// function numeroz (...numbers) {
+//     let numeroz = 0
+//     for (let alpha of numbers) {
+//         if (alpha > 0) {
+//             numeroz += alpha
+//         }
+//     }
+//     return numeroz; 
+// }
+
+// let positive = numeroz (2, -9, 5, 11, -30, 100, -8, -11, -4, 5, -6, 8);
+// console.log(positive);
+
+
+
+// -------------------------------------2-----------------------------------
+
+
+// function numberz (...numbers) {
+//     let all_numbers = 0
+//     for (let i of numbers) {
+//         all_numbers = all_numbers + i
+//     }
+// }
+// numberz(10, 50, 6, 7, 8, 11, 6, 3, 9); ????!
+
+
+
+// --------------------------------------3-------------------------------------
+
+// function giorgi_saakadze() {
+//     let user = {
+//         firstname: 'giorgi',
+//         lastname: 'saakadze',
+//         age: 32,
+//         isloggedin: true
+//     }
+// }  user-s ver kitxulobs ?!
+
+
+// ------------------------------------4----------------------------------------
+
+
+
+
+
+// -------------------------------5-------------------------------------
+
+// let ezpz = [1,2,4,10,34,5,7,87]
+
+// for (let x of ezpz) {
+//     if (x > 0 && x < 10) {
+//         console.log(x);
+//     }
+// }
+
+
+// --------------------------6------------------------------------------------------
+
+// let numbers =[1,2,3,4,5,6,7,8,9,10]
+
+// for (let y = 0; y < numbers.length; y++) {
+//     if (y <=5) {
+//         console.log(y);
+//     }
+// }
+
+  
